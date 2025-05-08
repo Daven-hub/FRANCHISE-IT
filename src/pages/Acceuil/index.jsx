a0f108b5f63a627dc7648175d0a8cc3eb497e9d9
richard-55123 <richardhemba55@gmail.com>
2025-05-08 15:52:06 +0100

    constructs

--- src/pages/Acceuil/index.jsx
+++ src/pages/Acceuil/index.jsx
@@ -14,7 +14,7 @@ const Accueil = () => {
     <div className="bg-bg-end relative overflow-hidden">
       <div className="absolute inset-0 z-0 opacity-30">
         <div className="absolute inset-0 bg-grid-blue-900/[0.02]"></div>
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,...')]"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA2MCA2MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxyZWN0IHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgZmlsbD0iI2VlZjJmZiIvPjxwYXRoIGQ9Ik0zMCAxMGMyMC4wMDUgMCAzMCA5Ljk5NSAzMCAzMHMtOS45OTUgMzAtMzAgMzBTMCA1MC4wMDUgMCAzMCA5Ljk5NSAxMCAzMCAxMHptMCAyMGMxMS4wNDYgMCAyMCA4Ljk1NCAyMCAyMHMtOC45NTQgMjAtMjAgMjBTMTAgNDEuMDQ2IDEwIDMwczguOTU0LTIwIDIwLTIweiIgZmlsbD0iI2QwZDRmZiIvPjwvZz48L3N2Zz4=')]"></div>
       </div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-5 relative z-10 space-y-16">
