423e52344b9111037d123cd1190cddf7240214a6
richard-55123 <richardhemba55@gmail.com>
2025-05-08 15:27:51 +0100

    fit

--- src/pages/Acceuil/index.jsx
+++ src/pages/Acceuil/index.jsx
@@ -1,9 +1,11 @@
+// pages/Accueil/index.jsx
+import 'animate.css';
 import About from "../../components/About";
 import FormulaireContact from "../../components/ContactComponent/FormulaireContact";
 import servicesData from "../../data/ServiceData/Index";
-import NotreEquipe from "../Equipes";
 import NosServices from "./CarroucelService";
 import RealisationsSection from "./RealisationSection";
+import AnimateOnScroll from '../../components/AnimateOnScroll';
 
 const service = servicesData.services;
 
@@ -12,24 +14,27 @@ const Accueil = () => {
     <div className="bg-bg-end relative overflow-hidden">
       <div className="absolute inset-0 z-0 opacity-30">
         <div className="absolute inset-0 bg-grid-blue-900/[0.02]"></div>
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA2MCA2MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxyZWN0IHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgZmlsbD0iI2VlZjJmZiIvPjxwYXRoIGQ9Ik0zMCAxMGMyMC4wMDUgMCAzMCA5Ljk5NSAzMCAzMHMtOS45OTUgMzAtMzAgMzBTMCA1MC4wMDUgMCAzMCA5Ljk5NSAxMCAzMCAxMHptMCAyMGMxMS4wNDYgMCAyMCA4Ljk1NCAyMCAyMHMtOC45NTQgMjAtMjAgMjBTMTAgNDEuMDQ2IDEwIDMwczguOTU0LTIwIDIwLTIweiIgZmlsbD0iI2QwZDRmZiIvPjwvZz48L3N2Zz4=')]"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,...')]"></div>
       </div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-5 relative z-10">
-        <div>
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-5 relative z-10 space-y-16">
+
+        <AnimateOnScroll animation="animate__fadeInUp">
           <NosServices services={service} />
-        </div>
-        <div>
-          <About></About>
-        </div>
-        <div>
+        </AnimateOnScroll>
+
+        <AnimateOnScroll animation="animate__fadeInLeft">
+          <About />
+        </AnimateOnScroll>
+
+        <AnimateOnScroll animation="animate__zoomIn">
           <RealisationsSection />
-        </div>
-         <div>
-          {/* <NotreEquipe/> */}
-         </div>
-        <div>
+        </AnimateOnScroll>
+
+        <AnimateOnScroll animation="animate__fadeInUp">
           <FormulaireContact />
-        </div>
+        </AnimateOnScroll>
+        
       </div>
     </div>
   );
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,7 +3,11 @@ import FormulaireContact from "../../components/ContactComponent/FormulaireConta
 const Contact = () => {
   return (
     <>
-      <div className="bg-bg-end ">
+      <div className="relative overflow-hidden">
+      <div className="absolute inset-0 z-0 opacity-30">
+        <div className="absolute inset-0 bg-grid-blue-900/[0.02]"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA2MCA2MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxyZWN0IHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgZmlsbD0iI2VlZjJmZiIvPjxwYXRoIGQ9Ik0zMCAxMGMyMC4wMDUgMCAzMCA5Ljk5NSAzMCAzMHMtOS45OTUgMzAtMzAgMzBTMCA1MC4wMDUgMCAzMCA5Ljk5NSAxMCAzMCAxMHptMCAyMGMxMS4wNDYgMCAyMCA4Ljk1NCAyMCAyMHMtOC45NTQgMjAtMjAgMjBTMTAgNDEuMDQ2IDEwIDMwczguOTU0LTIwIDIwLTIweiIgZmlsbD0iI2QwZDRmZiIvPjwvZz48L3N2Zz4=')]"></div>
+      </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 ">
           <FormulaireContact />
         </div>
